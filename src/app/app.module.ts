b024660ca9faa0354b0649e563b616f3757ce247
Gilles <gilles.gardet@gmail.com>
2021-05-31 14:02:23 +0200

    feat: use timeline as a resume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ResumeComponent } from './resume/resume.component';
+import {CardModule} from "primeng/card";
+import {TimelineModule} from "primeng/timeline";
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ResumeComponent
   ],
   imports: [
     AppRoutingModule,
+    ButtonModule,
     BrowserAnimationsModule,
-    BrowserModule
+    BrowserModule,
+    CardModule,
+    TimelineModule
   ],
   bootstrap: [AppComponent]
 })
