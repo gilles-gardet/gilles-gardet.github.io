88cf8773474d51f9f3ab67abc8a19af4e3d3f8bb
gilles-gardet <gilles.gardet@gmail.com>
2022-04-30 14:37:11 +0000

    Deploying to pages from @ gilles-gardet/gilles-gardet.github.io@4771c2ff530078735b00e1a8aca039e93a8066ac 🚀

--- src/app/core/core.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ConfigService } from '@core/services/config.service';
-
-@NgModule({
-  imports: [CommonModule],
-  providers: [ConfigService],
-})
-export class CoreModule {}
--- src/app/shared/components/slide-button/slide-button.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'portfolio-slide-button',
-  templateUrl: './slide-button.component.html',
-  styleUrls: ['./slide-button.component.scss'],
-})
-export class SlideButtonComponent {}
