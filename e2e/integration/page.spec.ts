8ed3e3dfe36ac2e811cf61a524b65b4dd64d5db4
Gilles <gilles.gardet@gmail.com>
2022-05-18 15:51:26 +0200

    feat: stylize and break the content into pages on print

--- e2e/integration/page.spec.ts
+++ e2e/integration/page.spec.ts
@@ -9,7 +9,16 @@ test.describe('Page structure', () => {
     await expect(page).toHaveURL('http://localhost:4200');
     await expect(page).toHaveTitle('Gilles Gardet');
   });
+
   test('Should have a panel "profil et généralités"', async ({ page }) => {
-    await expect(page.locator('text=Profil et généralités')).toBeVisible();
+    await expect(page.locator('.p-panel-title:has-text("Profil et généralités")')).toBeVisible();
+  });
+
+  test('Should have a panel "langages et outils"', async ({ page }) => {
+    await expect(page.locator('.p-panel-title:has-text("Langages et outils")')).toBeVisible();
+  });
+
+  test('Should have a panel "expérience"', async ({ page }) => {
+    await expect(page.locator('.p-panel-title:has-text("Expérience")')).toBeVisible();
   });
 });
